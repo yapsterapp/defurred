b69a02ff473c9483d8e9efef640d157e289ab242
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-10 11:30:09 +0100

    move clj dep to dev profile

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/employeerepublic/defurred"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [manifold "0.1.1-alpha3"]
-                 [funcool/cats "0.6.1"]])
+  :dependencies [[manifold "0.1.1-alpha3"]
+                 [funcool/cats "0.6.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}})
