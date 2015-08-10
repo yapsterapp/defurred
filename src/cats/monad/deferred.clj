43d6eb29280a8f3030d49fffb9a116cacdcc780b
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-10 13:57:38 +0100

    fix error propagation and convert values to Either

--- src/cats/monad/deferred.clj
+++ src/cats/monad/deferred.clj
@@ -2,7 +2,15 @@
   (:require [cats.core :as m]
             [cats.context :as ctx]
             [cats.protocols :as proto]
+            [cats.monad.either :as either]
             [manifold.deferred :as d]))
+
+(defn with-value
+  "wrap a value in a Deferred"
+  [v]
+  (let [d (d/deferred)]
+    (d/success! d v)
+    d))
 
 (def ^{:no-doc true}
   deferred-monad
@@ -11,20 +19,17 @@
     (fmap [mn f mv]
       (let [ctx (ctx/get-current mv)
             d (d/deferred)]
-        (d/chain mv (fn [v]
-                      (d/success!
-                       d
-                       ;; Set double monad to properly handle
-                       ;; monad transformers
-                       (m/with-monad ctx
-                         (f v)))))
+        (d/on-realized
+         mv
+         (fn [v] (d/success!
+                  d
+                  (m/with-monad ctx (f v))))
+         (fn [x] (d/error! d x)))
         d))
 
     proto/Applicative
     (pure [_ v]
-      (let [d (d/deferred)]
-        (d/success! d v)
-        d))
+      (with-value v))
 
     (fapply [mn af av]
       (d/chain af
@@ -33,26 +38,37 @@
 
     proto/Monad
     (mreturn [_ v]
-      (let [d (d/deferred)]
-        (d/success! d v)
-        d))
+      (with-value v))
 
     (mbind [mn mv f]
-      (let [ctx (ctx/get-current mv)
-            d (d/deferred)]
-        (d/chain mv (fn [v]
-                      (m/with-monad ctx
-                        (d/chain (f v)
-                                 (fn [nv]
-                                   (d/success! d nv))))))
-        d))))
+      (let [ctx (ctx/get-current mv)]
+        (d/chain mv
+                 (fn [v]
+                   (m/with-monad ctx
+                     (f v))))))))
 
 (extend-type manifold.deferred.Deferred
   proto/Context
   (get-context [_] deferred-monad))
 
-(defn with-value
-  [v]
-  (let [d (d/deferred)]
-    (d/success! d v)
-    d))
+(extend-type manifold.deferred.SuccessDeferred
+  proto/Context
+  (get-context [_] deferred-monad))
+
+(extend-type manifold.deferred.ErrorDeferred
+  proto/Context
+  (get-context [_] deferred-monad))
+
+(defn either-deferred
+  "convert the Deferred error handling model to
+   an Either value"
+  ([d] (either-deferred d identity))
+  ([d error-transformer]
+   (let [ed (d/deferred)]
+     (d/on-realized
+      d
+      (fn [v] (either/right v))
+      (fn [x] (either/left (error-transformer x)))))))
+
+(def either-deferred-monad
+  (either/either-transformer deferred-monad))
