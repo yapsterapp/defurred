88fcc910982ef67e775b1721feac75f227639082
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-18 14:14:20 +0200

    use cats release

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[manifold "0.1.1-alpha3"]
-                 [funcool/cats "0.7.0-SNAPSHOT"]]
+                 [funcool/cats "0.6.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}})
