8acc4378fadab63498fe237410cda6520e290e4d
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-28 22:22:10 +0100

    update manifold dep

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject defurred "0.1.1"
+(defproject defurred "0.1.2"
   :description "A Manifold Deferred monad for Cats"
   :url "https://github.com/employeerepublic/defurred"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[manifold "0.1.1-alpha3"]
+  :dependencies [[manifold "0.1.1-alpha4"]
                  [funcool/cats "0.6.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}})
